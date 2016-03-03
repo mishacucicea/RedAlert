8913860c9316a3b4a9c53059ad77b866c27355bc
sgrabovschi <sergiu.grabovschi@endava.com>
2016-03-03 18:28:11 +0200

    Added create Identity functionality

---
+++ API/RedAlert.API/RedAlert.API/BL/DeviceIdentity.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace RedAlert.API.BL
+{
+    public class DeviceIdentity
+    {
+        RegistryManager registryManager;
+        string connectionString;
+        string _deviceId;
+       
+
+        public DeviceIdentity(string deviceId)
+        {
+            _deviceId = deviceId;
+            AddDeviceAsync().Wait();
+        }
+        private async Task AddDeviceAsync()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["IotHubConnectionString"].ConnectionString;
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            Device device = null;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(_deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                throw new Exception("Device Alredy Exist");
+            }   
+        }
+        public async Task<string> GetDeviceAsync(string deviceId)
+        {
+            var device = new Device();
+            try
+            {
+                 device = await registryManager.GetDeviceAsync(deviceId);
+            }
+
+            catch (DeviceNotFoundException)
+            {
+                throw new Exception("Device Does not Exist");
+            }
+
+           return device.Authentication.SymmetricKey.PrimaryKey;
+        }
+
+
+    }
+}
---
+++ API/RedAlert.API/RedAlert.API/Controllers/DeviceIdentityController.cs
@@ -0,0 +1,35 @@
+﻿using RedAlert.API.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace RedAlert.API.Controllers
+{
+    public class DeviceIdentityController : ApiController
+    {
+
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get(string deviceId)
+        {
+           HttpResponseMessage response;
+           
+            try
+            {
+                response = Request.CreateResponse(HttpStatusCode.Created);
+                DeviceIdentity device = new DeviceIdentity(deviceId);
+                var deviceKey = await device.GetDeviceAsync(deviceId);
+                response.Content = new StringContent(deviceKey);
+            }
+            catch
+            {
+                response= Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+            return response;
+        }
+    }
+}
