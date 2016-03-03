91b176aa4327d76f92379269d84a974f85966dea
sergius0301 <sergiu0301>
2016-03-03 23:41:24 +0200

    Added new controller for MVC and view for adding new device

---
+++ API/RedAlert.API/RedAlert.API/Controllers/DeviceController.cs
@@ -0,0 +1,29 @@
+﻿using RedAlert.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedAlert.API.Controllers
+{
+    public class DeviceController : Controller
+    {
+        // GET: Device
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(DeviceModel model)
+        {
+            return View();
+        }
+
+        public ActionResult Get()
+        {
+            return View();
+        }
+    }
+}
---
+++ API/RedAlert.API/RedAlert.API/Models/DeviceModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedAlert.API.Models
+{
+    public class DeviceModel
+    {
+        public string SerialNumber { get; set; }
+        public string DeviceKey { get; set; }
+    }
+}
