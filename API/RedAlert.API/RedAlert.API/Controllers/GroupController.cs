c9796805de3aada2809d27ef29f09ec7b413c318
sgrabovschi <sergiu.grabovschi@endava.com>
2016-03-05 14:58:40 +0200

    added group

---
+++ API/RedAlert.API/RedAlert.API/Controllers/GroupController.cs
@@ -0,0 +1,31 @@
+﻿using RedAlert.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedAlert.API.Controllers
+{
+    public class GroupController : Controller
+    {
+        //
+        // GET: /Group/
+        public ActionResult Create(DeviceGroup model)
+        {
+            var name = "test";
+            var password = "pass";
+            var list = new List<DeviceModel>();
+            if (name == model.Name && password == model.Password)
+            {
+                ViewData["result"] = "success";
+            }
+            return View();
+        }
+        public ActionResult Get(DeviceGroup model)
+        {
+           
+            return View();
+        }
+	}
+}
---
+++ API/RedAlert.API/RedAlert.API/Models/DeviceGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RedAlert.API.Models
+{
+    public class DeviceGroup
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public List<string> SerialNumbers { get; set; }
+    }
+}
