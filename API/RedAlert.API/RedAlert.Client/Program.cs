ef52a153bf46a8e2ae699833926100df219335e7
mishacucicea <mishacucicea@yahoo.com>
2016-03-05 18:03:12 +0200

    Commit client and publish profile.

---
+++ API/RedAlert.API/RedAlert.Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace RedAlert.Client
+{
+    class Program
+    {
+        /// <summary>
+        /// Mains the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            using (var mySerialPort = new SerialPort("COM6"))
+            {
+                mySerialPort.Open();
+
+                while (true)
+                {
+                    var input = Console.ReadLine();
+
+                    if (input == "q")
+                    {
+                        break;
+                    }
+
+                    mySerialPort.Write(input);
+                }
+            }
+        }
+    }
+}
