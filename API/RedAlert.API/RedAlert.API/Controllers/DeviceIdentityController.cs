148a9199b4b077885f5efbc23875f49fa99cf88a
sgrabovschi <sergiu.grabovschi@endava.com>
2016-05-17 10:06:41 +0300

    Small refactoring and modified the message method

--- API/RedAlert.API/RedAlert.API/Controllers/DeviceIdentityController.cs
+++ API/RedAlert.API/RedAlert.API/Controllers/DeviceIdentityController.cs
@@ -1,11 +1,7 @@
 ﻿using RedAlert.API.BL;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
 
 namespace RedAlert.API.Controllers
