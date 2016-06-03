c9f87e51fc76ac7a471a9a6542c24190d6c0556b
sgrabovschi <sergiu.grabovschi@endava.com>
2016-06-03 17:26:04 +0300

    added receipe

---
+++ API/RedAlert.API/RedAlert.API/Controllers/ReceipeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedAlert.API.Controllers
+{
+    public class ReceipeController : Controller
+    {
+        // GET: Receipe
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
