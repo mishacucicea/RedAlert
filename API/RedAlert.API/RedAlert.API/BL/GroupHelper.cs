073c36e3b7f4e01934ec8d22a7cf89602e4420bc
sgrabovschi <sergiu.grabovschi@endava.com>
2016-06-21 16:57:21 +0300

    added send to group

---
+++ API/RedAlert.API/RedAlert.API/BL/GroupHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RedAlert.API.Controllers;
+using RedAlert.API.Models;
+
+
+namespace RedAlert.API.BL
+{
+    public class GroupHelper
+    {
+        public static async void SendGroupMessage(List<Device> devices,string color)
+        {
+            MessageController controller = new MessageController();
+            foreach (var device in devices)
+            {
+                await controller.Send(device.SenderKey, color);
+            }
+            
+        }
+    }
+}
