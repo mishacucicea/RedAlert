6157a16ff545acbe131f44e1b9a77f6d450d6aa0
sgrabovschi <sergiu.grabovschi@endava.com>
2016-07-04 15:13:12 +0300

    Added Authentication

---
+++ API/RedAlert.API/RedAlert.API/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RedAlert.API.Startup))]
+namespace RedAlert.API
+{
+
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
