a82c15a60ed9a120351eda4ff7d8614924f549a1
sgrabovschi <sergiu.grabovschi@endava.com>
2016-07-07 12:34:44 +0300

    changed layout for url and adding color field

---
+++ API/RedAlert.API/RedAlert.API/Scripts/pages/generateurl.js
@@ -0,0 +1,33 @@
+﻿var clipboard = new Clipboard('.btn');
+
+$(function () {
+    var apiUrl = window.location.protocol + "//" + window.location.host;
+    var senderKey = localStorage.getItem("senderKey");
+    var color = localStorage.getItem("color");
+   
+
+    if (senderKey != null) {
+        $('#senderkeyid').val(senderKey);
+    }
+
+    if (senderKey != null) {
+        $('#textBoxUrl').val(apiUrl + '/api/message/send?senderkey=' + senderKey + '&color=' + color);
+    }
+    if (color == null || "") {
+        color = $('#colorboxId').val();
+    }
+    $("#colorboxId").change(function () {
+        var senderKeyBox = $('#senderkeyid').val();
+        var color = $('#colorboxId').val();
+        if (senderKeyBox != null && color!= null) {
+            $('#textBoxUrl').val(apiUrl + '/api/message/send?senderkey=' + senderKeyBox + '&color=' + color);
+        }
+    });
+   
+    $("#senderkeyid").change(function () {
+        var senderKeyBox = $('#senderkeyid').val();
+        if (senderKeyBox != null) {
+            $('#textBoxUrl').val(apiUrl + '/api/message/send?senderkey=' + senderKeyBox + '&color=' + color);
+        }
+    });
+});
