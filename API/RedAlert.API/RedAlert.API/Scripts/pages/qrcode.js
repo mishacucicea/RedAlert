d595947e5b5af4c5668ce04b44fb10575cc6418e
sgrabovschi <sergiu.grabovschi@endava.com>
2016-07-07 12:49:08 +0300

    added layout for qrCode

---
+++ API/RedAlert.API/RedAlert.API/Scripts/pages/qrcode.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    var senderKey = localStorage.getItem("senderKey");
+    var color = localStorage.getItem("color");
+    if (senderKey == null) {
+        $('#messageBox').text('Have you registered you device ?');
+    } else {
+        $('#senderkeyidQR').val(senderKey);
+        $("#qr-Image").attr("src", "/QrCode/GenerateQr?senderKey=" + senderKey + "&color=" + color);
+    }
+    $("#senderkeyidQR").change(function () {
+        var senderKeyBox = $('#senderkeyidQR').val();
+        if (senderKeyBox != null) {
+            $("#qr-Image").attr("src", "/QrCode/GenerateQr?senderKey=" + senderKeyBox + "&color=" + color);
+        }
+    });
+});
