00ed18f06f1e139a25aac8101b103253ca3e5752
sgrabovschi <sergiu.grabovschi@endava.com>
2016-07-08 14:20:48 +0300

    added layout for voting

---
+++ API/RedAlert.API/RedAlert.API/Scripts/pages/voting.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    var senderKey = localStorage.getItem("senderKey");
+    var senderKeyBox;
+
+    $("#qr-Imageup").attr("src", "/QrCode/GenerateVotingQr?answer=Up");
+
+    $("#qr-Imagedown").attr("src", "/QrCode/GenerateVotingQr?answer=Down");
+
+    if (senderKey == null) {
+        $('#messageBox').text('Have you registered you device ?');
+    } else {
+        $('#senderkeyidVotingQR').val(senderKey);
+        $.post("/Color/Voting?senderKey=" + senderKey);
+
+    }
+    $("#senderkeyidVotingQR").change(function () {
+        senderKeyBox = $('#senderkeyidVotingQR').val();
+        if (senderKeyBox != null) {
+            $.post("/Color/Voting?senderKey=" + senderKeyBox);
+        }
+    });
+
+    $("#resetButton").click(function () {
+
+        // it's assumed that a sender key is already in the local storage.
+        $.get("/Voting/Reset", {
+        });
+    });
+    setInterval(function () {
+        $.post("/Color/Voting?senderKey=" + senderKey);
+    }, 2000);
+});
