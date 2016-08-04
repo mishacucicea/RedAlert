43dce737cbf4e284f9981427794413b4cebd1ce1
Mihai Cucicea <mishacucicea@yahoo.com>
2016-08-04 16:37:59 +0300

    Temporary added AllowAnonymous on BaseController. Hub device Id is now hidden from device details.

--- API/RedAlert.API/RedAlert.API/Controllers/BaseController.cs
+++ API/RedAlert.API/RedAlert.API/Controllers/BaseController.cs
@@ -7,6 +7,7 @@ namespace RedAlert.API.Controllers
     /// Base class for all controllers.
     /// </summary>
     /// <seealso cref="System.Web.Mvc.Controller" />
+    [AllowAnonymous]
     public class BaseController : Controller
     {
         /// <summary>
